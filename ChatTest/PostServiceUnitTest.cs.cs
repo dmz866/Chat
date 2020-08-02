4748077da8f929c309c937f76cfaf33054da5f95
David Murillo <dmz866@hotmail.com>
2020-08-02 16:09:47 -0500

    unit testing added

---
+++ ChatTest/PostServiceUnitTest.cs.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Chat.Infrastructure.Services;
+using Chat.Core.Interfaces;
+using Chat.Core.Entities;
+using System.Threading.Tasks;
+
+namespace ChatTest
+{
+    /// <summary>
+    /// Summary description for PostServiceUnitTest
+    /// </summary>
+    [TestClass]
+    public class PostServiceUnitTest
+    {
+        IPostService _postService;
+        public PostServiceUnitTest()
+        {
+            _postService = new PostService();
+        }
+        [TestMethod]
+        public async Task TestCreatePost()
+        {
+            Post post = new Post();
+            post.CreatedAt = DateTime.Now;
+            post.Message = "TEST";
+            post.UserName = "TEST USER";
+            await _postService.Create(post);
+
+            Assert.IsTrue(true);
+        }
+    }
+}
---
+++ ChatTest/UtilsServiceUnitTest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Threading.Tasks;
+using Chat.Core.Entities;
+using Chat.Core.Interfaces;
+using Chat.Core.Utils;
+using Chat.Infrastructure.Services;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ChatTest
+{
+    [TestClass]
+    public class UtilsServiceUnitTest
+    {
+        IUtilsService _utilsService;
+        public UtilsServiceUnitTest()
+        {
+            _utilsService = new UtilsService();
+        }
+        [TestMethod]
+        public async Task TestGetStockInformation()
+        {
+            string stockCode = "aapl.us";
+            Stock result = await _utilsService.GetStockInformation(stockCode);
+            Assert.IsTrue(!string.IsNullOrEmpty(result.Symbol) && !result.Symbol.Equals(Constants.STOCK_API_ERROR_CODE));
+        }
+    }
+}
